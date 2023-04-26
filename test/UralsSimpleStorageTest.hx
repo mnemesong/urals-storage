18b66faa6e57981e27368b0d3d30ba3f6db00804
Pantagruel74 <tostar74@mail.ru>
2023-04-27 04:18:14 +0500

    Add test for Simple Storage

--- test/UralsSimpleStorageTest.hx
+++ test/UralsSimpleStorageTest.hx
@@ -37,8 +37,33 @@ function test2() {
     assert(readedAll[2].val == "alala");
 }
 
+function test3() {
+    var stor = new UralsSimpleStorage();
+    stor.setMany([{id: 2, val: "blablabla"}, {id: 3, val: "ohoho"}]);
+    var readedAll = stor.readMany([3]);
+    assert(readedAll[0].id == 3);
+    assert(readedAll[0].val == "ohoho");
+
+    stor.reInit(["alala", "blablabla", "ohoho"]);
+    var readedAll = stor.readAll();
+    assert(readedAll[0].id == 1);
+    assert(readedAll[0].val == "alala");
+    assert(readedAll[1].id == 2);
+    assert(readedAll[1].val == "blablabla");
+    assert(readedAll[2].id == 3);
+    assert(readedAll[2].val == "ohoho");
+
+    stor.reInit(["azaza", "gagaga"]);
+    var readedAll = stor.readAll();
+    assert(readedAll[0].id == 1);
+    assert(readedAll[0].val == "azaza");
+    assert(readedAll[1].id == 2);
+    assert(readedAll[1].val == "gagaga");
+}
+
 
 function main() {
     test1();
     test2();
+    test3();
 }
--- test/UralsStaticReactiveStorageTest.hx
+++ test/UralsStaticReactiveStorageTest.hx
@@ -82,8 +82,52 @@ function test2() {
         + 'gets(id: 1, val: alala)\n'));
 }
 
+function test3() {
+    var output = "";
+    var stor = new UralsReactiveStorageStub(
+        (els) -> {
+            output = output + els
+            .map(e -> 'sets(id: ${Std.string(e.id)}, val: ${e.val})\n')
+            .join("");
+        },
+        (els) -> {
+            output = output + els
+            .map(e -> 'gets(id: ${Std.string(e.id)}, val: ${e.val})\n')
+            .join("");
+        }
+    );
+    stor.reInit(["alala", "blablabla", "ohoho"]);
+    var readedAll = stor.readAll();
+    assert(readedAll[0].id == 1);
+    assert(readedAll[0].val == "alala");
+    assert(readedAll[1].id == 2);
+    assert(readedAll[1].val == "blablabla");
+    assert(readedAll[2].id == 3);
+    assert(readedAll[2].val == "ohoho");
+
+    stor.reInit(["azaza", "gagaga"]);
+    var readedAll = stor.readAll();
+    assert(readedAll[0].id == 1);
+    assert(readedAll[0].val == "azaza");
+    assert(readedAll[1].id == 2);
+    assert(readedAll[1].val == "gagaga");
+
+    assert(output == 
+        ( 'sets(id: 1, val: alala)\n' 
+        + 'sets(id: 2, val: blablabla)\n'
+        + 'sets(id: 3, val: ohoho)\n'
+        + 'gets(id: 1, val: alala)\n' 
+        + 'gets(id: 2, val: blablabla)\n'
+        + 'gets(id: 3, val: ohoho)\n'
+        + 'sets(id: 1, val: azaza)\n' 
+        + 'sets(id: 2, val: gagaga)\n'
+        + 'gets(id: 1, val: azaza)\n' 
+        + 'gets(id: 2, val: gagaga)\n'));
+}
+
 
 function main() {
     test1();
     test2();
+    test3();
 }
